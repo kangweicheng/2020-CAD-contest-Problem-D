c0e387253eb66f0fedaf4858bb70a2afc5e238f7
kangweicheng <b05505028@ntu.edu.tw>
2020-07-01 11:15:45 +0800

    push

--- data/parse.py
+++ data/parse.py
@@ -23,7 +23,29 @@ def loadFunction(func_Str, literal_index_List, index_literal_list):
 				function_represent .append(str(literal_index_List[i]))
 	function_represent = ('"%s"'%','.join(function_represent))
 	return function_represent, literal_index_List, index_literal_list
-
+# 0 is negative
+# 1 is positive
+# 2 is dont care
+# 3 is dont exist in the cell
+def loadWhen(When_Str, literal_index_List, index_literal_list, inputs):
+	print(When_Str)
+	whenLiteral = [3 for i in index_literal_list.keys()]
+	for i in inputs:
+		try:
+			whenLiteral[literal_index_List[i]] = 2
+		except:
+			None
+	if When_Str:
+		When_Str = When_Str[1:-1].split('&')
+		for i in When_Str:
+			if i[0] == '!':
+				literal = i[1:]
+				whenLiteral[literal_index_List[literal]] = 0
+			else :
+				literal = i
+				whenLiteral[literal_index_List[literal]] = 1
+	
+	return ('"%s"'%','.join([str(i) for i in whenLiteral]))
 
 
 
@@ -38,32 +60,36 @@ def parse(cells_list, timing_file, power_file, literal_index_List, index_literal
 				inPin, outPin, when = timing_pair
 				function = cell['output'][outPin]['function'][1:-1]
 				functionLiteral, literal_index_List, index_literal_list = loadFunction(function, literal_index_List, index_literal_list)
+
+				whenLiteral = loadWhen(when, literal_index_List, index_literal_list, cell['input'].keys())
 				for Type in [(0, 'fall_transition'), (1, 'rise_transition'), (2, 'cell_fall'), (3, 'cell_rise')]:
 					table = cell['timing'][timing_pair][Type[1]]
 					timing_transition_index = cell['timing_transition_index']
 					timing_capacitance_index = cell['timing_capacitance_index']
 					for transition, row in zip(timing_transition_index, table):
 						for capacitance, element in zip(timing_capacitance_index, row):
-							timing.write('%d,%f,%d,%f,%f,%d,%f,%d,%f,%f,%s\n'% (
+							timing.write('%d,%f,%d,%f,%f,%d,%f,%d,%f,%f,%s,%s\n'% (
 									timing_index, element, Type[0], transition, capacitance,
 									cell['process'], cell['voltage'], cell['temperature'],
-									cell['input'][inPin]['rise_capacitance'], cell['input'][inPin]['fall_capacitance'], functionLiteral
+									cell['input'][inPin]['rise_capacitance'], cell['input'][inPin]['fall_capacitance'], functionLiteral, whenLiteral
 								))
 							timing_index += 1
 			for power_pair in cell['internal_power'].keys():
 				inPin, outPin, when = power_pair
 				function = cell['output'][outPin]['function'][1:-1]
 				functionLiteral, literal_index_List, index_literal_list = loadFunction(function, literal_index_List, index_literal_list)
+				
+				whenLiteral = loadWhen(when, literal_index_List, index_literal_list, cell['input'].keys())
 				for Type in [(0, 'rise_power'), (1, 'fall_power')]:
 					table = cell['internal_power'][power_pair][Type[1]]
 					power_transition_index = cell['power_transition_index']
 					power_capacitance_index = cell['power_capacitance_index']
 					for transition, row in zip(power_transition_index, table):
 						for capacitance, element in zip(power_capacitance_index, row):
-							power.write('%d,%f,%d,%f,%f,%d,%f,%d,%f,%f,%s\n'% (
+							power.write('%d,%f,%d,%f,%f,%d,%f,%d,%f,%f,%s,%s\n'% (
 									power_index, element, Type[0], transition, capacitance,
 									cell['process'], cell['voltage'], cell['temperature'],
-									cell['input'][inPin]['rise_capacitance'], cell['input'][inPin]['fall_capacitance'], functionLiteral
+									cell['input'][inPin]['rise_capacitance'], cell['input'][inPin]['fall_capacitance'], functionLiteral, whenLiteral
 								))
 							power_index += 1
 	print({
--- data/preprocess.py
+++ data/preprocess.py
@@ -1,21 +1,27 @@
 import read
-import evaluator
+# import evaluator
 from parse import parse
+import pickle
 literal_index_List = {'pad': 0, '(': 1, ')': 2, '!': 3, '^': 4, '|': 5}
 index_literal_list = {0: 'pad', 1: '(', 2:')', 3:'!', 4: '^', 5: '|'}
-
-
-train_cells1 = read.get_libs("lib1_tt1p0v25c_base_400.tlib")
+try:
+	with open('../literal.plckle', 'rb') as f:
+		liter_dict = pickle.load(f)
+		literal_index_List = liter_dict['literal_index_List']
+		index_literal_list = liter_dict['index_literal_list']
+except:
+	None
+train_cells1 = read.get_libs("../lib1_tt1p0v25c_base_400.tlib")
 print('lib 1 is done')
-train_cells2 = read.get_libs("lib1_ss0p9vm40c_base_400.tlib")
+train_cells2 = read.get_libs("../lib1_ss0p9vm40c_base_400.tlib")
 print('lib 2 is done')
-train_cells3 = read.get_libs("lib1_ss0p9v125c_base_400.tlib")
+train_cells3 = read.get_libs("../lib1_ss0p9v125c_base_400.tlib")
 print('lib 3 is done')
-train_cells4 = read.get_libs("lib1_ff1p1vm40c_base_400.tlib")
+train_cells4 = read.get_libs("../lib1_ff1p1vm40c_base_400.tlib")
 print('lib 4 is done')
-train_cells5 = read.get_libs("lib1_ff1p1v125c_base_400.tlib")
+train_cells5 = read.get_libs("../lib1_ff1p1v125c_base_400.tlib")
 print('lib 5 is done')
 # test_lib, test_cells = read.get_libs("./release/partial/lib1_ff0p88v125c_alpha_100.lib")
-p = parse([train_cells1, train_cells2,train_cells3,train_cells4,train_cells5], './timing.csv', './power.csv', literal_index_List, index_literal_list)
+p = parse([train_cells1], './timing.csv', './power.csv', literal_index_List, index_literal_list)
 
 # print(evaluator.evaluator(train_cells, train_cells))
